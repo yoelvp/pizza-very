6b308b2c83eab49ae271c9bcf12587feccd70141
yoelvp <yoelvp73@gmail.com>
2024-10-17 17:53:58 -0500

    feat: intial commit

---
+++ app/src/main/java/com/ngcraftz/pizzadelivery/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ngcraftz.pizzadelivery;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        SharedPreferences sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
+        boolean isLogged = sharedPreferences.getBoolean("isLogged", false);
+        // if (isLogged) setContentView(R.layout.activity_main);
+        // if (!isLogged) setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
